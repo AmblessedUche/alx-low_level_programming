0df9c1f757b60763cc02e46550c18ce63a0f808a
Amblessed <princeamblessed@gmail.com>
2023-02-23 17:36:15 -0800

    Finished

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,13 +5,14 @@
  * @n: int type number
  * Description: Can only use _putchar to print.
  */
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10)
-        print_number(n / 10);
-    _putchar(n % 10 + '0');
+void print_number(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+	if (n / 10)
+	print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
-
