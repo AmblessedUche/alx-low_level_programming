39fbe3a54f324440ca5decaa57e581af255453d4
Amblessed <princeamblessed@gmail.com>
2023-02-24 03:23:46 -0800

    Done deal

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * print_times_table - Prints a multiplication table up to param
@@ -10,41 +10,32 @@ void print_times_table(int n)
 {
 	int x, y, z;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
 		for (x = 0; x <= n; x++)
 		{
-			for (y = 0; y <= n; y++)
+			_putchar('0');
+			for (y = 1; y <= n; y++)
 			{
+				_putchar(',');
+				_putchar(' ');
 				z = x * y;
-				if (z > 99)
+				if (z <= 99)
+				_putchar(' ');
+				if (z <= 9)
+				_putchar(' ');
+				if (z >= 100)
 				{
-					_putchar(',');
-					_putchar(32);
 					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(((z / 10)) % 10 + '0');
 				}
-				else if (z > 9)
+				else if (z <= 99 && z >= 10)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
+					_putchar((z / 10) + '0');
+				}
 					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
 			}
+
 			_putchar('\n');
 		}
 	}
