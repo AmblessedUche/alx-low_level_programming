0d118d54f4eee549dceee8caf20952f60f59a11e
Amblessed <princeamblessed@gmail.com>
2023-04-04 21:02:53 -0700

    Updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,14 +13,15 @@ size_t print_listint(const listint_t *h)
 	size_t count = 1;
 
 	if (h == NULL)
-		return (0);
+	return (0);
 
 	while (h->next != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+	printf("%d\n", h->n);
+	h = h->next;
+	count++;
 	}
 	printf("%d\n", h->n);
+
 	return (count);
 }
